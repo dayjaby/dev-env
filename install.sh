aad988acea830ea3749d60a2bf64695b73205fd3
dayjaby <dayjaby@gmail.com>
2019-02-28 13:11:15 +0100

    added vundle

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 rsync -av home/ $HOME/
-mkdir -p ~/.local/bin
-cd ~/.local/bin
-git clone https://github.com/nojhan/liquidprompt.git
-cd -
+git clone https://github.com/nojhan/liquidprompt.git ~/.local/bin/liquidprompt
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "source $HOME/.local/rc/.profile" >> ~/.profile
 echo "source $HOME/.local/rc/.bashrc" >> ~/.bashrc
 vim +PluginInstall +qall
